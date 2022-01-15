ad81f7d07a33135263a7b1a6c5ca8af02b6d1f8f
MaagMichael <63949690+MaagMichael@users.noreply.github.com>
2022-01-15 12:17:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ app = FastAPI()
 def message():
     return {"message": "This is a test with Heraku service API"}
 
+# Get current date and time from Server
 @app.get("/now")
 def now():
     now = datetime.datetime.now()
     now = now.strftime("Today %d.%m.%Y - now %H:%M")
     return {"Time":now}
 
+# Create BMI calc, request as "/bmicalconly?height=2&weight=77"
+@app.get("/bmicalconly")
+async def calc_bmi(height: float, weight: float) -> float: 
+    bmi = round(weight/ (height*height),2)
+    return {"data": "The calculated BMI is: {}".format(bmi)} # no html page rendered as response !
 
 
 if __name__ == "__main__":
