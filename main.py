e8d560b28152e06274354cf42bac8b93be2c8542
MaagMichael <63949690+MaagMichael@users.noreply.github.com>
2022-01-25 19:39:53 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles 
+from fastapi.responses import HTMLResponse # only for case 2
+
 import datetime
 app = FastAPI()
 
+# depending on your working directory of your virtual enviroment ! here "BMIcalcJinJa2"
+templates = Jinja2Templates(directory="./templates")
+app.mount("/static", StaticFiles(directory="./static"), name="static")
 
 @app.get("/")
 def message():
@@ -21,6 +28,12 @@ async def calc_bmi(height: float, weight: float) -> float:
     bmi = round(weight/ (height*height),2)
     return {"data": "The calculated BMI is: {}".format(bmi)} # no html page rendered as response !
 
+#++++++++++++++++++ HTML response +++++++++++++++++++++++++++++++ request as "/bmicalchtml/2,77"
+
+@app.get("/bmicalchtml/{height},{weight}", response_class=HTMLResponse)
+async def calc_bmi(request: Request, height: float, weight: float):
+    result = round(weight/ (height*height),2)
+    return templates.TemplateResponse("bmi_result.html", {"request": request, "bmi": result})
 
 if __name__ == "__main__":
     uvicorn.run("main:app")
